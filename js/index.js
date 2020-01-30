c62776eeada7826db05fdd5e954b8d1193ff299a
EuphoriaCHN <879969355@qq.com>
2020-01-31 04:11:31 +0800

    完善第三个页面的布局及动画

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
 ((window, undefined) => {
     $(function () {
+        let windowHeight = $(window).height();
+        let flag = false; // 控制动画
+
         $('#fullpage').fullpage({
             navigation: true,
             loopBottom: true,
             scrollingSpeed: 1200,
 
             // 回调函数
+
+            // 在滚动到第 index 个页面时调用
             afterLoad: function (anchorLink, index) {
                 switch (index) {
                     case 2:
+                        if (flag) break;
                         new Promise(resolve => {
                             // 将搜索框展示出来
                             $('.search').fadeIn(700, function () {
@@ -47,17 +53,47 @@
                                 }, 200);
                             });
                         }).then(value => {
-                            // 然后再让“沙发”的搜索结果变大
-                            value.animate({
-                                'height': 218
-                            }, 600);
+                            return new Promise(resolve => {
+                                // 然后再让“沙发”的搜索结果变大
+                                value.animate({
+                                    'height': 218
+                                }, 600, () => resolve());
+
+                                // 让“沙发”变大的同时，替换掉 words-01 的 description
+                                $('.words-01').slideUp(500);
+                                // 同时让 words-02 出现
+                                $('.words-02').slideDown(500);
+                            });
                         }).then(() => {
-                            // 让“沙发”变大的同时，替换掉 words-01 的 description
-                            $('.words-01').slideUp(500);
-                            // 同时让 words-02 出现
-                            $('.words-02').slideDown(500);
+                            // 当“沙发”的搜索结果变大后，用单独的“沙发”图片和 cover 覆盖掉那个会掉下去的特殊的沙发
+                            $('.shirt-cover').show();
+                            $('.shirt-02').show();
+                            flag = true;
                         });
                         break;
+                }
+            },
+
+            // 在刚刚滚动出去某个页面时触发
+            onLeave: function (index, nextIndex, direction) {
+                if (index === 2 && nextIndex === 3 && flag) {
+                    new Promise(resolve => {
+                        // 从第二个聘屏幕滑动到第三个屏幕时
+                        // 位于第二个屏幕的沙发会掉下去
+                        $('.shirt-02').animate({
+                            'bottom': -(windowHeight - 280),
+                            'width': 207,
+                            'left': 260
+                        }, 2000, () => resolve());
+                    }).then(() => {
+                        // 沙发落下来后，改变第三屏购物车和颜色选择框的背景
+                        $('.img-01-a').fadeIn(500);
+                        $('.btn-01-a').fadeIn(1000);
+                    });
+                } else if (index === 3 && nextIndex === 4) {
+                    // 从第三个屏幕到第四个屏幕时
+                    $('.shirt-02').hide();
+                    $('.t1f').show();
                 }
             },
         });
